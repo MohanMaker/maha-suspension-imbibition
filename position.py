66eca231d36b656bdf0b39f747256df188eb1f01
MohanMaker <mohanH123456@gmail.com>
2023-11-09 02:11:23 -0500

    improve formatting, add blur

--- position.py
+++ position.py
@@ -1,72 +1,60 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 
-# define an array of integers to hold the min x location of the vertical bar in every frame
-min_x_locations = []
-def analysis_darkest(frame):
-    # Convert to grayscale
-    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+def analyze_liquid_front(frame):
+    # Convert to grayscale and blur the image
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blurred_frame = cv2.GaussianBlur(gray_frame, (3, 3), 0)
 
-    # Compress each column of pixels into one pixel by taking the median, which excludes outliers
-    compressed_frame = np.median(frame_gray, axis=0)
+    # Median of each column to represent the column
+    compressed_frame = np.median(blurred_frame, axis=0)
 
-    # Calculate the derivative of the compressed frame
-    derivative = np.diff(compressed_frame)
+    # Find the sharpest change in intensity
+    intensity_change = np.diff(compressed_frame)
+    liquid_front_location = np.argmin(intensity_change)
 
-    # Find the location of the largest spike in the derivative
-    # We use np.argmin here because a spike (large negative change) indicates the presence of the front
-    spike_locs = np.where(derivative == np.amin(derivative))[0]
-    if spike_locs.size > 0:
-        min_x_loc = spike_locs[0]  # Taking the first spike location
-        min_x_locations.append(min_x_loc)
-        # Draw a line at the position of the detected front
-        frame_marked = cv2.line(frame.copy(), (min_x_loc, 0), (min_x_loc, frame.shape[0]), (255, 0, 0), 2)
-    else:
-        frame_marked = frame.copy()
-        min_x_locations.append(np.nan)  # Append NaN to indicate no spike was found
+    # Mark the liquid front location on the frame
+    marked_frame = cv2.line(frame.copy(), (liquid_front_location, 0),
+                            (liquid_front_location, frame.shape[0]), (255, 0, 0), 2)
 
-    return frame_marked
+    return marked_frame, liquid_front_location
 
-# Open the video file.
-video_path = "media/DSC_0038_cropped2.avi"
-cap = cv2.VideoCapture(video_path)
-
-# Check if the video file was opened successfully.
-if not cap.isOpened():
-    print("Error: Could not open video.")
-    exit()
-
-while True:
-    # Read a frame from the video.
-    ret, frame = cap.read()
-
-    # If 'ret' is False, it means the video has ended.
-    if not ret:
-        print(min_x_locations)
+def main(video_path):
+    # Stores the liquid front location for each frame
+    liquid_front_locations = []
     
-        # plot the min x locations over time
-        x = np.arange(len(min_x_locations))
-        min_x = np.array(min_x_locations)
-        plt.plot(x, min_x)
-        plt.xlabel('Frame Number')
-        plt.ylabel('Min Grayscale X Location')
-        plt.title('Min Grayscale X Location vs. Frame Number')
-        plt.show()
+    # Video capture
+    cap = cv2.VideoCapture(video_path)
+    if not cap.isOpened():
+        print("Error: Could not open video.")
+        return
 
-        break
+    # Frame by frame analysis
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        
+        marked_frame, location = analyze_liquid_front(frame)
+        liquid_front_locations.append(location)
+        cv2.imshow('Video Playback', marked_frame)
 
-    # Use the analysis function that works with the darkest point
-    # and extract only the image from the returned tuple for displaying
-    marked_frame = analysis_darkest(frame)
+        # Exit on 'q' keypress
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
 
-    # Display the frame.
-    cv2.imshow('Video Playback', marked_frame)
+    # Cleanup
+    cap.release()
+    cv2.destroyAllWindows()
 
-    # Wait for 25ms and check if the user pressed 'q' to exit.
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        break
+    # Plot results
+    plt.plot(liquid_front_locations)
+    plt.xlabel('Frame Number')
+    plt.ylabel('Liquid Front X Location')
+    plt.title('Liquid Front X Location vs. Frame Number')
+    plt.show()
 
-# Release the video capture object and close all OpenCV windows.
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    video_path = "media/DSC_0036_cropped2.avi"
+    main(video_path)
