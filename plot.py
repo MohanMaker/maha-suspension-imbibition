32705cbc5a49e57b9ab051dd26b90b273ddc921c
MohanMaker <mohanH123456@gmail.com>
2024-02-27 20:47:05 -0500

    redo scaling

--- plot.py
+++ plot.py
@@ -20,9 +20,9 @@ def read_csv_and_plot(csv_filename):
     # Log-Log Plot
     plt.loglog(frame_numbers, liquid_front_locations, linestyle="None", marker='o', markersize=3, label='Data')
     
-    y_equals_x = np.array(frame_numbers) * 80
+    y_equals_x = np.array(frame_numbers) * 5
     plt.loglog(frame_numbers, y_equals_x, label='y = x', linestyle='--', color='green')
-    y_sqrt_x = np.sqrt(frame_numbers) * 120
+    y_sqrt_x = np.sqrt(frame_numbers) * 40
     plt.loglog(frame_numbers, y_sqrt_x, label='y = sqrt(x)', linestyle='--', color='red', zorder=0)
 
     plt.xlabel('Frame Number')
@@ -32,5 +32,5 @@ def read_csv_and_plot(csv_filename):
     plt.show()
 
 if __name__ == "__main__":
-    csv_filename = "media/022724-water-zoomedout-offset108.csv"
+    csv_filename = "media/022724-glycerol.csv"
     read_csv_and_plot(csv_filename)
